d1e4fab7f076bcca86314f09af808483489efc95
mawood <mawoodmain@googlemail.com>
2018-03-14 12:23:29 +0000

    Added uptime segment to persistence

--- src/main/java/me/mawood/loraCapture/packet/segment/segments/UptimeSegment.java
+++ src/main/java/me/mawood/loraCapture/packet/segment/segments/UptimeSegment.java
@@ -6,6 +6,10 @@ import me.mawood.loraCapture.packet.block.blocks.IntBlock;
 import me.mawood.loraCapture.packet.segment.InvalidSegmentException;
 import me.mawood.loraCapture.packet.segment.Segment;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "uptime_segments")
 public class UptimeSegment extends Segment
 {
     public static final byte[] TYPE_FLAG = {0x00, 0x04};
@@ -16,11 +20,22 @@ public class UptimeSegment extends Segment
         if(!(blocks[0] instanceof IntBlock)) throw new InvalidSegmentException("First block should be a int block");
     }
 
+    public UptimeSegment() {
+    }
+
+    @Basic
+    @Column(name = "uptime")
     public int getUptime()
     {
         return ((IntBlock) blocks[0]).getData(); //Instant.ofEpochMilli(((IntBlock) blocks[0]).getData());
     }
 
+    public void setUptime(int uptime)
+    {
+        //TODO implement setting block
+    }
+
+    @Transient
     @Override
     public JsonObject toJson()
     {
@@ -29,12 +44,14 @@ public class UptimeSegment extends Segment
         return json;
     }
 
+    @Transient
     @Override
     public String getJsonName()
     {
         return "Uptime";
     }
 
+    @Transient
     @Override
     public String toString()
     {
